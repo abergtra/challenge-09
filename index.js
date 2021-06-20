841ca7c302bb4a57519ce041f431c01c5151515e
Asher <asher.bergtraun@gmail.com>
2021-06-20 11:07:58 -0700

    built out question section

--- index.js
+++ index.js
@@ -7,7 +7,90 @@
     const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //Title
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter a project title.",
+        validate: validateInput,
+    },
+    //Description
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project.",
+        validate: validateInput,
+    },
+    
+    //Table of Contents does not need User Input
+    
+    //Installation
+    {
+        type: "input",
+        name: "installation",
+        message: 'Please provide installation instructions. If this section is not applicable, please write "none"',
+        validate: validateInput,
+    },
+    //Usage
+    {
+        type: "input",
+        name: "usage",
+        message: "How should this progam or project be used?",
+        validate: validateInput,
+    },
+    //License (these are selected from a list)
+    {
+        type: "list",
+        name: "license",
+        message: "Please pick a license from the following list:",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Apache 2.0",
+            "Boost Software 1.0",
+            "MIT",
+            "Mozilla",
+        ],
+        validate: validateInput,
+    },
+    //Contributing
+    {
+        type: "input",
+        name: "contributing",
+        message: "How should users contribute?",
+        validate: validateInput,
+    },
+    //Tests
+    {
+        type: "input",
+        name: "tests",
+        message: 'Please provide testing instructions. If this section is not applicable, please write "none"',
+        validate: validateInput,
+    },
+    //Questions (need multiple inputs for this section)
+        //Email Address
+        {
+            type: "input",
+            name: "emailAddress",
+            message: "What is your email address?",
+            validate: function(value) {
+                if (condition) {
+                    return true;
+                } else {
+                    return "This email is not valid. Please check spelling and enter a valid address."
+                }
+            },
+        },
+        //GitHub Username
+        {
+            type: "input",
+            name: "GithubUsername",
+            message: "What is your GitHub username?",
+            validate: validateInput,
+        },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
