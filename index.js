3b31dc4e60409f19a2d88aed736bc3b447811664
Asher <asher.bergtraun@gmail.com>
2021-06-20 20:04:48 -0700

    functions: write readme & initialize app

--- index.js
+++ index.js
@@ -121,10 +121,22 @@ function getLicense(value) {
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+        data.getLicense = getLicense(data.license);
+        writeToFile("./example/README.md", data);
+    });
+}
 
 // Function call to initialize app
 init();
